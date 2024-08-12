798a15e164e24e31769ccac073da289ddeb1a31f
Gil Rudge <gilblas07@hotmail.com>
2024-08-12 20:58:57 -0300

    criação de rota getSelectedArenaCtrl

---
+++ controller/getSelectedArenaCtrl.js
@@ -0,0 +1,29 @@
+const Arena = require('../models/Arena');
+
+
+const getSelectedArenaCtrl = async (req, res) => {
+
+  try {
+    
+    const {idArena} = req.params
+
+    const arenaExists = await Arena.findById(idArena);
+
+    if(!arenaExists){
+
+      return res.status(404).json({message: "Arena não encontrada"})
+
+    };
+
+    res.status(201).json({message: "Arena encontrada", arena: arenaExists})
+
+  } catch (error) {
+
+      console.log(error.message);
+
+      res.status(500).json({message: "Ocorreu um erro na requisição"})
+  }
+
+};
+
+module.exports = getSelectedArenaCtrl;
--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,8 @@ const createArenaCtrl = require('../controller/createArenaCtrl');
 const updateVideosArenaCtrl = require('../controller/updateVideosArenaCtrl');
 const getArenasCtrl = require('../controller/getArenasCtrl');
 const deleteVideoArenaCtrl = require('../controller/deleteVideoArenaCtrl');
-const deleteArenaCtrl = require('../controller/deleteArenaCtrl')
+const deleteArenaCtrl = require('../controller/deleteArenaCtrl');
+const getSelectedArenaCtrl = require('../controller/getSelectedArenaCtrl');
 
 
 //Public routes
@@ -20,12 +21,13 @@ router.post('/auth/login', loginUserCtrl);
 //Private route users
 // router.get('/user/:id', checkToken, userPrivateRouteCtrl);
 router.get('/arenas',checkToken, getArenasCtrl);
+router.get('/arenas/:idArena',checkToken, getSelectedArenaCtrl);
 
 //Private routes admin
 router.post('/cadastro-arena', createArenaCtrl);
+router.delete('/exclui-arena/:idArena', deleteArenaCtrl);
 router.put('/grava-lances',updateVideosArenaCtrl);
 router.delete('/exclui-lances/:idArena/:idVideo', deleteVideoArenaCtrl);
-router.delete('/exclui-arena/:idArena', deleteArenaCtrl);
 
 
 
