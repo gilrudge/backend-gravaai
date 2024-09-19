1b8310276c7847223b524e33be0dc5687351be75
Gil Rudge <gilblas07@hotmail.com>
2024-09-18 21:46:16 -0300

    alteração getArenasInfoCtrl

--- controller/getArenasInfoCtrl.js
+++ controller/getArenasInfoCtrl.js
@@ -6,25 +6,25 @@ const getArenasInfoCtrl = async (req, res) => {
   try {
     
     
-    const arena = await Arena.aggregate([{
-      $project: {
-        _id: 1,
-        nomeArena: 1,
-        videos: {
-          $filter: {
-            input: '$videos',
-            as: 'video',
-            cond: { $eq: ['$$video.flg_process', true] }
-          }
-        }
-      }
-    },
-    {
-      $match: {
-        'videos.0': { $exists: true }
-      }
-    }]);
-
+    // const arena = await Arena.aggregate([{
+    //   $project: {
+    //     _id: 1,
+    //     nomeArena: 1,
+    //     videos: {
+    //       $filter: {
+    //         input: '$videos',
+    //         as: 'video',
+    //         cond: { $eq: ['$$video.flg_process', true] }
+    //       }
+    //     }
+    //   }
+    // },
+    // {
+    //   $match: {
+    //     'videos.0': { $exists: true }
+    //   }
+    // }]);
+    const arena = await Arena.find();
 
     res.status(200).json({ arenas: arena });
 
